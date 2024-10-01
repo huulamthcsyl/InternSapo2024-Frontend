9e58281cc3ddbb3fcb4a24afc5e503bddc268ea9
noname112003 <119551107+noname112003@users.noreply.github.com>
2024-10-01 13:16:28 +0700

    Update MainBox.tsx

--- src/components/layout/MainBox.tsx
+++ src/components/layout/MainBox.tsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import { Box } from '@mui/material'
 import Toolbar from '@mui/material/Toolbar'
+import {ToastContainer} from "react-toastify";
 
 type Props = {
-  children: React.ReactNode
+    children: React.ReactNode
 }
 
 export default function MainBox({ children }: Props) {
-  return (
-    <Box component="main" sx={{ backgroundColor: '#F0F1F1', height: '100vh' }}>
-      <Toolbar />
-      {children}
-    </Box>
-  )
+    return (
+        <Box component="main" sx={{ backgroundColor: '#F0F1F1'}}>
+            <Toolbar />
+            {children}
+            <ToastContainer hideProgressBar autoClose={3000} />
+        </Box>
+    )
 }
