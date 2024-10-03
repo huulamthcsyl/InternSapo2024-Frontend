83aa70294341ee682c53e314cdc628f90dd09b8a
quang <quangteo7112003@gmail.com>
2024-10-04 00:33:34 +0700

    submit code

--- src/pages/customer/CustomerPageAppBar.tsx
+++ src/pages/customer/CustomerPageAppBar.tsx
@@ -1,22 +1,39 @@
-import { Typography, Box } from '@mui/material'
+import {Typography, Box, Select, MenuItem} from '@mui/material'
 import MainAppBar from '../../components/layout/MainAppBar'
+import {useEffect, useState} from "react";
+import {useNavigate} from "react-router-dom";
 
 
 type Props = {}
 
 export default function CustomerPageAppBar({}: Props) {
-  return (
-    <MainAppBar >
-      <Box sx={{ display: 'flex' }}>
-        <Typography variant="h6"  sx={{
-            color: '#000', // Màu chữ
-            fontFamily: '"Segoe UI", sans-serif', // Phông chữ
-            fontSize: '26px', // Kích thước chữ
-            fontStyle: 'normal', // Kiểu chữ
-            fontWeight: 600, // Độ đậm của chữ
-            lineHeight: 'normal', // Chiều cao dòng
-        }}  >Khách hàng</Typography>
-      </Box>
-    </MainAppBar>
-  )
+    const [user, setUser] = useState<any>(null);
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        localStorage.removeItem('token');
+        localStorage.removeItem('user');
+        navigate('/login');
+    }
+
+    useEffect(() => {
+        const user = localStorage.getItem('user');
+        setUser(user ? JSON.parse(user) : null);
+    }, []);
+
+    return (
+        user && <MainAppBar>
+            <Box sx={{ display: 'flex', flexGrow: 1, justifyContent: 'space-between', alignItems: 'center' }}>
+                <Typography variant="h5" sx={{ color: '#000', fontWeight: '600' }}>Danh sách khách hàng</Typography>
+                <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                    <Typography variant="h6" sx={{ color: '#000', fontWeight: '600' }}>{user.name}</Typography>
+                    <Select sx={{ '.MuiOutlinedInput-notchedOutline': { borderStyle: 'none' } }}>
+                        <MenuItem onClick={() => navigate(`/account/${user.id}`)}>Thông tin tài khoản</MenuItem>
+                        <MenuItem onClick={handleLogout}>Đăng xuất</MenuItem>
+                    </Select>
+                </Box>
+            </Box>
+        </MainAppBar>
+    )
+
 }
